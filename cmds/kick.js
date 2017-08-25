01df76499463afc0ec01f4614167e33164d088e3
Duale Siad <dmanrazor@hotmail.com>
2017-08-25 10:14:28 +1000

    Added mod commands

---
+++ cmds/kick.js
@@ -0,0 +1,25 @@
+module.exports.run = async (bot, message, args) => {
+    if(!message.member.roles.some(r=>["Administrator", "Moderator"].includes(r.name)) )
+      return message.reply("Sorry, you don't have permissions to use this!");
+    
+ 
+    let member = message.mentions.members.first();
+    if(!member)
+      return message.reply("Please mention a valid member of this server");
+    if(!member.kickable) 
+      return message.reply("I cannot kick this user! Do they have a higher role? Do I have kick permissions?");
+    
+    
+    let reason = args.slice(1).join(' ');
+    if(!reason)
+      return message.reply("Please indicate a reason for the kick!");
+    
+    await member.kick(reason)
+      .catch(error => message.reply(`Sorry ${message.author} I couldn't kick because of : ${error}`));
+    message.reply(`${member.user.tag} has been kicked by ${message.author.tag} because: ${reason}`);
+
+}
+
+module.exports.help  = {
+    name: "kick"
+}
--- cmds/serverinfo.js
+++ cmds/serverinfo.js
@@ -1,11 +1,13 @@
 const Discord = require('discord.js');
 module.exports.run = async (bot, message, args) => {
        let servembed = new Discord.RichEmbed()
+       let guild = new Discord.Guild()
             .setDescription("This server's info!")
             .setColor("#ff0000")
-            .addField("Guild name", message.guild.name)
-            .addField("Guild ID", message.guild.id)
-            .addField("Guild created at", message.guild.createdAt);
+            .addField("Guild name", guild.name)
+            .addField("Guild ID", guild.id)
+            .addField("Guild created at", guild.createdAt)
+            .addField("Guild Owner", guild.owner)
             
        message.channel.send({servembed});     
 }
