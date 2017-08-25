551a17fab38f3ca84e6094808643133e1cba047b
Duale Siad <dmanrazor@hotmail.com>
2017-08-25 11:50:00 +1000

    Updated Help Message

--- cmds/help.js
+++ cmds/help.js
@@ -5,8 +5,9 @@ message.channel.send("Check your private messages, " + message.author.mention)
         .addField("`pp2!userinfo`", "Your user info")
         .addField("`pp2!avatar`", "Generates your avatar")
         .addField("`pp2!help`", "Shows this message")
-        .addField("`pp2!ping`", "Shows Discord's WebSocket ping")
-        .addField("These are the commands so far, join my server (https://discord.gg/qz4jxzb) to keep updated.", "Thanks for using my bot!");
+        .addField("`pp2!serverinfo`", "Shows server info" )
+        .addField("These are the commands so far, join my server (https://discord.gg/qz4jxzb) to keep updated.", "Invite: https://bit.ly/PowerPlug");
+
 
 message.author.send({embed: embed});
 
