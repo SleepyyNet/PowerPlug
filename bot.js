2a7dda4c18b07f71e4c1ab0c211f5b82c00d22cd
Duale Siad <dmanrazor@hotmail.com>
2017-08-31 20:55:42 +1000

    Added base level system

--- bot.js
+++ bot.js
@@ -2,6 +2,8 @@ const settings = require('./settings.json');
 const Discord = require('discord.js');
 const fs = require('fs');
 
+let points = JSON.parse(fs.readFileSync("./levels/points.json", "utf8"));
+
 const prefix = settings.prefix;
 
 const bot = new Discord.Client({disableEveryone: true})
@@ -13,7 +15,7 @@ bot.commands = new Discord.Collection();
 
     let jsfiles = files.filter(f => f.split(".").pop() === "js");
     if(jsfiles.length <= 0) {
-        console.log("No commands to load");
+        console.log("No main commands to load");
         return;
     }
 
@@ -31,11 +33,11 @@ bot.commands = new Discord.Collection();
 
     let jsfiles = files.filter(f => f.split(".").pop() === "js");
     if(jsfiles.length <= 0) {
-        console.log("No commands to load");
+        console.log("No level commands to load");
         return;
     }
 
-    console.log(`${jsfiles.length} main commands to load.`)
+    console.log(`${jsfiles.length} level-related commands to load.`)
 
     jsfiles.forEach((f, i) => {
         let props = require(`./levels/${f}`);
@@ -48,7 +50,7 @@ bot.commands = new Discord.Collection();
 bot.on("ready", async () => {
     console.log(`PowerPlug is plugged in! Username: ${bot.user.username}`);
     bot.user.setAvatar("https://cdn.discordapp.com/attachments/350173567675006977/350173647081570324/Unbenannt1.png");
-    bot.user.setStatus("pp2!help for help")
+    bot.user.setGame('pp2!help for help')
     try {
         let link = await bot.generateInvite(["ADMINISTRATOR"]);
         let github = "https://github.com/codefromcirc/powerplugjs";
@@ -74,7 +76,30 @@ bot.on("message", async message => {
    let cmd = bot.commands.get(command.slice(prefix.length));
    if(cmd) cmd.run(bot, message, args);
 
+   if (!points[message.author.id]) points[message.author.id] = {
+    points: 0,
+    level: 0
+  };
+
+    let userData = points[message.author.id];
+    userData.points++;
+
+    
+    let curLevel = Math.floor(0.1 * Math.sqrt(userData.points));
+    if (curLevel > userData.level) {
+        userData.level = curLevel;
+        message.reply(`You"ve leveled up to level **${curLevel}**! How does that feel?`);
+    }
+    if (message.content.startsWith(prefix + "level")) {
+        message.reply(`You are currently level ${userData.level}, with ${userData.points} points.`);
+    }
+    
+    fs.writeFile("./levels/points.json", JSON.stringify(points), (err) => {
+    if (err) console.error(err)
+
     });
+
+  });
 });
 
 bot.login(settings.token)
