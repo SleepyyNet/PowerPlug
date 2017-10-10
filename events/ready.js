207f7d7b807fda83028283f818b86cb3a98a5494
CircuitRCAY <dmanrazor@hotmail.com>
2017-10-10 12:08:09 +1100

    fixed ready event

--- events/ready.js
+++ events/ready.js
@@ -3,7 +3,8 @@ const snek = require('snekfetch');
 exports.run = async (client) => {
     client.user.setStatus(`${settings.prefix}help for help`);
     snek.post(`https://discordbots.org/api/bots/${client.user.id}/stats`)
-        .set("Authorization", settings.keys.dbl)
-        .send({"server_count":client.guilds.size})
-        .then(console.log("Server count at DBL updated!"));
+    .set('Authorization', 'YOUR DISCORDBOTS.ORG API TOKEN')
+    .send({ server_count: client.guilds.size })
+    .then(() => console.log('Updated discordbots.org stats.'))
+    .catch(err => console.error(`Whoops something went wrong: ${err.body}`));
 }
